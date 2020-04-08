4061d8af6dd219fc6755eb6b0b11a3af82270fa3
Yogiraj <yogiraj.kadikar@gmail.com>
2020-04-09 01:21:40 +0530

    some updates

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,23 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(w):
-    return data[w]
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        print( "Did you mean %s instead." % get_close_matches(w, data.keys())[0])
+        a= input( "Please Enter 'Y' or 'N'.: ")
+        if a == 'Y':
+            return word
+        else:
+            pass
+    else:
+        print("Word entered by You is wrong. Please re-check it.")
 
-word = input("Enter a word: ")
+word = input("Enter word: ")
 
 print(translate(word))
 
